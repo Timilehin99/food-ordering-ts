feee5a09a68d2050958dc7b4a18e89c0f446720d
Timilehin99 <timilearnsprogramming@gmail.com>
2024-05-04 20:29:05 +0100

    Creating Project structure

---
+++ controllers/vendorControllers.ts
@@ -0,0 +1,13 @@
+import {Request, Response, NextFunction} from "express";
+
+export const createVendor = async (req: Request, res: Response, next: NextFunction) =>{
+
+}
+
+export const getVendor = async (req: Request, res: Response, next: NextFunction) =>{
+
+}
+
+export const getVendorID = async (req: Request, res: Response, next: NextFunction) =>{
+
+}
