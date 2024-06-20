790b9fe338989041cc5e10a1a09974ee4be9ab0d
Timilehin99 <timilearnsprogramming@gmail.com>
2024-06-20 16:44:35 +0100

    Tweaking gitigore

--- config/index.ts
+++
@@ -1,6 +0,0 @@
-const MONGODB_URL = "mongodb://127.0.0.1:27017/myapp";
-const APP_SECRET = "MY_little_PoNy"
-const TWILIO_SID = "Placeholder"
-const TWILIO_AUTH = "PlaceHolder"
-
-export{MONGODB_URL, APP_SECRET, TWILIO_AUTH, TWILIO_SID}
